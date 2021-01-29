6f81081ab68da54bed408c93943c2cc5524acfad
Ian <ifransperalta@gmail.com>
2021-01-29 11:56:10 -0500

    Lab 2 in progress

--- app/src/main/java/com/example/androidlab/MainActivity.java
+++ app/src/main/java/com/example/androidlab/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.androidlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getResources().getString(R.string.toast_message);
+
+        CheckBox box = (CheckBox)findViewById(R.id.checkbox);
+        box.onCheckChangeListener((CompoundButton cb, boolean b) -> {
+            Snackbar snackbar = Snackbar.setAction("Undo", click ->cb.setChecked(b));
+        });
     }
 }
