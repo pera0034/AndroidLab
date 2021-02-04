0f2ca84b95ec7852879ea30bb983a38b343a0a0f
Ian <ifransperalta@gmail.com>
2021-02-03 23:20:59 -0500

    Finished lab 3

--- app/src/main/java/com/example/androidlab/MainActivity.java
+++ app/src/main/java/com/example/androidlab/MainActivity.java
@@ -2,24 +2,56 @@ package com.example.androidlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getResources().getString(R.string.toast_message);
+        setContentView(R.layout.activity_login);
 
-        CheckBox box = (CheckBox)findViewById(R.id.checkbox);
+        Intent profilePage = new Intent(this, ProfileActivity.class);
+        profilePage.putExtra("email", "qwerty@site.com");
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(click -> startActivity( profilePage ));
 
-        box.setOnCheckedChangeListener((CompoundButton cb, boolean b) -> {
-            /*Snackbar.setAction is having an error.*/
-            Snackbar snackbar = Snackbar.setAction("Undo", click ->cb.setChecked(b));
-        });
+        SharedPreferences sharedPref = getSharedPreferences(
+        getString(R.id.text1EmailAddress), Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPref.edit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/androidlab/ProfileActivity.java
@@ -0,0 +1,86 @@
+package com.example.androidlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+    ImageButton mImageButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        /*Intent dataSent = getIntent();
+        String nameSent = dataSent.getStringExtra("email");
+
+        Button previousButton = findViewById(R.id.login);
+        previousButton.setOnClickListener(click -> {
+            setResult(1 ,dataSent);
+            finish();
+        });*/
+
+        ImageButton imgButton = (ImageButton) findViewById(R.id.imageButton);
+        Log.e("onCreate", "Activity: onCreate()");
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart", "In function: onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("onPause", "In function: onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume", "In function: onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("onStop", "In function: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("onDestroy", "In function: onDestroy()");
+    }
+}
